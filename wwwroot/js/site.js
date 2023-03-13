32dd5b8fbefcae835c83ca6c3846866319f0f911
joshthecomposer <110321887+joshthecomposer@users.noreply.github.com>
2023-03-13 15:57:28 -0400

    No animations yet, working on dashboard navbar

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,16 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿let navItems = document.querySelectorAll(".nav-item")
+let show = false;
 
-// Write your JavaScript code.
+function toggleNav()
+{
+    if (!show)
+    {
+        navItems.forEach(i => i.style.display = "inline");
+        show = true;
+    }
+    else
+    {
+        navItems.forEach(i => i.style.display = "none");
+        show = false;
+    }
+}
