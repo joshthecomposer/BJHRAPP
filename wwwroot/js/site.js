6d8c0fdf34a8b26f225e1cfb3037ea3fe4529d86
joshthecomposer <110321887+joshthecomposer@users.noreply.github.com>
2023-03-14 14:15:55 -0400

    dashboard layout

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,20 +1,3 @@
 ﻿let navItems = document.querySelectorAll(".nav-item");
 let navBar = document.querySelector("nav");
 
-
-navBar.onmouseenter = function () {
-    setTimeout(() => {
-        navItems.forEach(i => {
-            i.style.opacity = "1.0"
-            i.style.display = "inline"
-        })
-    }, 400)
-}
-navBar.onmouseleave = function () {
-    setTimeout(() => {
-        navItems.forEach(i => {
-            i.style.opacity = "0"
-            i.style.display = "none"
-        })
-    }, 100)
-}
