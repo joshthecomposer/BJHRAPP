1bd6f164e2621dc82fcea079eaec6b5912bf1b47
joshthecomposer <110321887+joshthecomposer@users.noreply.github.com>
2023-04-16 17:01:03 -0400

    remove ClaimCheckAttribute from SessionCheckAttribute file

--- Utilities/SessionCheckAttribute.cs
+++ Utilities/SessionCheckAttribute.cs
@@ -14,17 +14,3 @@ public class SessionCheckAttribute : ActionFilterAttribute
         }
     }
 }
-
-public class ClaimCheck : ActionFilterAttribute
-{
-    public override void OnActionExecuting(ActionExecutingContext filterContext)
-    {
-        string idString = (string)filterContext.RouteData.Values["UserId"]!;
-        int inputId = int.Parse(idString);
-
-        if (filterContext.HttpContext.Session.GetInt32("UserId") != inputId)
-        {
-            filterContext.Result = new RedirectResult("/");
-        }
-    }
-}
