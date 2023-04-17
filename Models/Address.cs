64f05d53552e66c43793cb5ff94a4445c078f3b8
joshthecomposer <110321887+joshthecomposer@users.noreply.github.com>
2023-04-17 01:45:52 -0400

    cleanup

--- Models/Address.cs
+++ Models/Address.cs
@@ -1,6 +1,5 @@
 #pragma warning disable CS8618
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BJHRApp.Models;
 public class Address : BaseEntity
